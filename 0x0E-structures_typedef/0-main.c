10218d102f5be7ec5340f8c9d3d46c40668b8403
lihleh <ntulicebelihle03@gmail.com>
2023-10-17 07:49:27 -0700

    dog.h

--- 0x0E-structures_typedef/0-main.c
+++ 0x0E-structures_typedef/0-main.c
@@ -13,6 +13,6 @@ struct dog my_dog;
 my_dog.name = "Poppy";
 my_dog.age = 3.5;
 my_dog.owner = "Bob";
-printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+printf("My name is %s, I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
 return (0);
 }
